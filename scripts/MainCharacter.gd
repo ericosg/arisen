31a3509ce28db1166976a92660b65b8b14093667
ericosg-dp <eric@loadx.org>
2025-05-13 18:49:40 +0300

    better cost displays

--- scripts/MainCharacter.gd
+++ scripts/MainCharacter.gd
@@ -1,18 +1,20 @@
 # MainCharacter.gd
 extends Node
-@onready var level_label       : Label  = $"../UI/Level"
-@onready var max_de_label      : Label  = $"../UI/MaxDarkEnergy"
-@onready var current_de_label  : Label  = $"../UI/DarkEnergy"
-@onready var mastery_label     : Label  = $"../UI/MasteryPoints"
-@onready var reanimate_button  : Button = $"../UI/Reanimate"
-@onready var soul_drain_button : Button = $"../UI/SoulDrain"
+@onready var level_label              : Label  = $"../UI/Level"
+@onready var max_de_label             : Label  = $"../UI/MaxDarkEnergy"
+@onready var current_de_label         : Label  = $"../UI/DarkEnergy"
+@onready var mastery_label            : Label  = $"../UI/MasteryPoints"
+@onready var reanimate_button         : Button = $"../UI/Reanimate"
+@onready var soul_drain_button        : Button = $"../UI/SoulDrain"
+@onready var reanimate_upgrade_button : Button = $"../UI/ReanimateUpgrade"
+@onready var soul_drain_upgrade_button : Button = $"../UI/SoulDrainUpgrade"
 
 var level        : int = 1
 var max_base_de  : int = 1
 var max_bonus_de : int = 0
 var max_de       : int = 1
 var current_de   : int = 1
-var mastery      : int = 1
+var mastery      : int = 0
 var spells : Dictionary[int, Spell] = {}
 
 func _ready() -> void:
@@ -41,25 +43,29 @@ func _update_de() -> void:
 
 func _update_ui() -> void:
 	level_label.text       = "Level: %d" % level
-	current_de_label.text  = "DE: %d" % current_de
-	max_de_label.text      = "MAX DE: %d" % max_de
-	mastery_label.text     = "Mastery: %d" % mastery
-	reanimate_button.text  = "REANIMATE %d" % spells[Spell.SpellType.REANIMATE].level
-	soul_drain_button.text = "SOUL DRAIN %d" % spells[Spell.SpellType.SOUL_DRAIN].level
+	current_de_label.text  = "Dark Energy (DE): %d" % current_de
+	max_de_label.text      = "Max DE: %d" % max_de
+	mastery_label.text     = "Mastery Points (MP): %d" % mastery
 	
-	var reanimate_cost = spells[Spell.SpellType.REANIMATE].get_mastery_cost()
-	var soul_drain_cost = spells[Spell.SpellType.SOUL_DRAIN].get_mastery_cost()
+	var reanimate_cost = spells[Spell.SpellType.REANIMATE].get_de_cost()
+	var soul_drain_cost = spells[Spell.SpellType.SOUL_DRAIN].get_de_cost()
 	
-	var reanimate_upgrade_button = $"../UI/ReanimateUpgrade"
-	var soul_drain_upgrade_button = $"../UI/SoulDrainUpgrade"
+	reanimate_button.text = "REANIMATE %d (%d DE)" % [spells[Spell.SpellType.REANIMATE].level, reanimate_cost]
+	soul_drain_button.text = "SOUL DRAIN %d (%d DE)" % [spells[Spell.SpellType.SOUL_DRAIN].level, soul_drain_cost]
+	
+	reanimate_button.disabled = current_de < reanimate_cost
+	soul_drain_button.disabled = current_de < soul_drain_cost
+	
+	var reanimate_mastery_cost = spells[Spell.SpellType.REANIMATE].get_mastery_cost()
+	var soul_drain_mastery_cost = spells[Spell.SpellType.SOUL_DRAIN].get_mastery_cost()
 	
 	if reanimate_upgrade_button:
-		reanimate_upgrade_button.text = "Upgrade (%d MP)" % reanimate_cost
-		reanimate_upgrade_button.disabled = mastery < reanimate_cost
+		reanimate_upgrade_button.text = "Upgrade (%d MP)" % reanimate_mastery_cost
+		reanimate_upgrade_button.disabled = mastery < reanimate_mastery_cost
 	
 	if soul_drain_upgrade_button:
-		soul_drain_upgrade_button.text = "Upgrade (%d MP)" % soul_drain_cost
-		soul_drain_upgrade_button.disabled = mastery < soul_drain_cost
+		soul_drain_upgrade_button.text = "Upgrade (%d MP)" % soul_drain_mastery_cost
+		soul_drain_upgrade_button.disabled = mastery < soul_drain_mastery_cost
 
 func _show_not_enough_de_popup() -> void:
 	print_debug("Not enough DE!")
